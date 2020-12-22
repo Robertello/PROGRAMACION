e9990d465cca2eccd72de3dd961deac25beaae89
Roberto Gonzalez <robertello@hotmail.com>
2020-12-22 11:49:59 +0100

    Cambiado a inepto

--- DAW 01- Hola mundo/src/daw2021/Daw2021.java
+++ DAW 01- Hola mundo/src/daw2021/Daw2021.java
@@ -15,7 +15,7 @@ public class Daw2021 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            System.out.println("Hola Mundo feo");
+            System.out.println("Hola Mundo inepto");
         // TODO code application logic here
     }
     

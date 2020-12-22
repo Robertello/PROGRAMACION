ed860f21e6685bc05c6a186c7201794ffe99f0b5
Roberto Gonzalez <robertello@hotmail.com>
2020-12-22 11:39:16 +0100

    Cambiado a denuevo

--- DAW 01- Hola mundo/src/daw2021/Daw2021.java
+++ DAW 01- Hola mundo/src/daw2021/Daw2021.java
@@ -15,7 +15,7 @@ public class Daw2021 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            System.out.println("Hola Mundo mio");
+            System.out.println("Hola Mundo denuevo");
         // TODO code application logic here
     }
     
